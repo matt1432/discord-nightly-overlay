dbe9ba7d9dc9fd99993f3dd5c2f5d7ee72ce68dd
matt1432 <matt@nelim.org>
2024-07-01 17:17:13 -0400

    chore: init flake

---
+++ README.md
@@ -0,0 +1,3 @@
+# Discord Nightly Overlay
+
+This is a Nix flake containing Discord packages to bring you the latest version as fast as possible.
---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  inputs = {
+    nixpkgs = {
+      type = "github";
+      owner = "NixOS";
+      repo = "nixpkgs";
+      ref = "nixos-unstable";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }: let
+    supportedSystems = [
+      "x86_64-linux"
+      "x86_64-darwin"
+      "aarch64-linux"
+      "aarch64-darwin"
+    ];
+
+    perSystem = attrs:
+      nixpkgs.lib.genAttrs supportedSystems (system:
+        attrs system nixpkgs.legacyPackages.${system});
+  in {
+    packages =
+      perSystem (system: pkgs:
+        import ./pkgs {inherit self system pkgs;});
+
+    formatter = perSystem (_: pkgs: pkgs.alejandra);
+
+    devShells = perSystem (_: pkgs: {
+      update = pkgs.mkShell {
+        packages = with pkgs; [
+          alejandra
+          bash
+          common-updater-scripts
+          git
+          jq
+          nix-prefetch-git
+          nix-prefetch-github
+          nix-prefetch-scripts
+        ];
+      };
+    });
+  };
+}
