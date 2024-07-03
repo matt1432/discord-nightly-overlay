83a67b2b7b411c0d351bb5236d6ded5a5b1efd85
matt1432 <matt@nelim.org>
2024-07-02 20:56:05 -0400

    fix: use the same name as the ones in nixpkgs

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,8 +4,8 @@
   system,
   ...
 }: {
+  discord = pkgs.callPackage ./discord/default.nix {branch = "stable";};
   discord-canary = pkgs.callPackage ./discord/default.nix {branch = "canary";};
   discord-development = pkgs.callPackage ./discord/default.nix {branch = "development";};
   discord-ptb = pkgs.callPackage ./discord/default.nix {branch = "ptb";};
-  discord-stable = pkgs.callPackage ./discord/default.nix {branch = "stable";};
 }
