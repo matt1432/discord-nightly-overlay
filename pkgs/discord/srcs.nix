86c671cb772f267b8a88fd550dc4579382928b41
Updater <robot@nowhere.invalid>
2024-08-10 07:06:36 +0000

    ci: discord-ptb 0.0.98 -> 0.0.99

--- pkgs/discord/srcs.nix
+++ pkgs/discord/srcs.nix
@@ -7,9 +7,9 @@
     };
 
     ptb = rec {
-      version = "0.0.98";
+      version = "0.0.99";
       url = "https://dl-ptb.discordapp.net/apps/linux/${version}/discord-ptb-${version}.tar.gz";
-      hash = "sha256-aJx5ruZRCnBTf/Qon1uFfxMX2a25Xg2AzbnN/UtDe8w=";
+      hash = "sha256-xXBWD5H4MOC2HaJwN5+N4s5fHrHet6zTm1C7ChWN8Zc=";
     };
 
     canary = rec {
